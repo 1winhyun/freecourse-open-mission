2faad2b37112c62d50557116c98712569a61bdf0
1winhyun <jimking1@naver.com>
2025-11-13 21:37:38 +0900

    feat: 오픈미션 시작

---
+++ src/main/kotlin/com/techcourse/openmission/OpenmissionApplication.kt
@@ -0,0 +1,11 @@
+package com.techcourse.openmission
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class OpenmissionApplication
+
+fun main(args: Array<String>) {
+	runApplication<OpenmissionApplication>(*args)
+}
---
+++ src/test/kotlin/com/techcourse/openmission/OpenmissionApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.techcourse.openmission
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class OpenmissionApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
