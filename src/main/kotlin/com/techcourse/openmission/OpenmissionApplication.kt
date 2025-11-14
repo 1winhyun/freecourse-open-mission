2dcb104852ea1b21afd7fd0f1d99698015dc2b22
1winhyun <jimking1@naver.com>
2025-11-15 00:15:29 +0900

    feat: 프로그램 실행 코드 작성

--- src/main/kotlin/com/techcourse/openmission/OpenmissionApplication.kt
+++ src/main/kotlin/com/techcourse/openmission/OpenmissionApplication.kt
@@ -1,11 +1,29 @@
 package com.techcourse.openmission
 
+import com.techcourse.openmission.lotto.controller.LottoController
+import com.techcourse.openmission.racingcar.controller.RacingController
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
-@SpringBootApplication
 class OpenmissionApplication
 
-fun main(args: Array<String>) {
-	runApplication<OpenmissionApplication>(*args)
+fun main() {
+    println("실행할 프로그램의 번호를 선택하세요.")
+    println("1: Lotto")
+    println("2: RacingCar")
+
+    when (readln().trim()) {
+        "1" -> {
+            val lottoController = LottoController()
+            lottoController.run()
+        }
+
+        "2" -> {
+            val racingController = RacingController()
+            racingController.run()
+        }
+        else -> {
+            println("잘못된 번호입니다. 프로그램을 종료합니다.")
+        }
+    }
 }
