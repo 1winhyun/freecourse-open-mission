47d1f8e0e9f0bdd9ccbfc3ac6756397cb939ecf7
1winhyun <jimking1@naver.com>
2025-11-14 23:55:36 +0900

    feat: LottoController 구현

---
+++ src/main/kotlin/com/techcourse/openmission/lotto/controller/LottoController.kt
@@ -0,0 +1,26 @@
+package com.techcourse.openmission.lotto.controller
+
+import com.techcourse.openmission.lotto.service.LottoService
+import com.techcourse.openmission.lotto.view.LottoInput
+import com.techcourse.openmission.lotto.view.LottoOutput
+
+class LottoController {
+    private val lottoInput = LottoInput()
+    private val lottoOutput = LottoOutput()
+    private val lottoService = LottoService()
+
+    fun run() {
+        val money = lottoInput.inputMoney()
+
+        val lottos = lottoService.butLotto(money)
+        lottoOutput.printBoughtLotto(lottos)
+
+        val winningNumber = lottoInput.inputWinningAndBonusNumber()
+
+        val result = lottoService.winnerConfirm(lottos, winningNumber)
+        val yield = lottoService.calculateYield(result, money)
+
+        lottoOutput.printStats(result, yield)
+    }
+
+}
