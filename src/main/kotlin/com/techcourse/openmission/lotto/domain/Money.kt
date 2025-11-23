d3b3a9b351fbecae4d2435fd33852c02b9e8eaac
1winhyun <jimking1@naver.com>
2025-11-23 18:24:45 +0900

    refactor: 구입한 금액 검증 로직 적용

--- src/main/kotlin/com/techcourse/openmission/lotto/domain/Money.kt
+++ src/main/kotlin/com/techcourse/openmission/lotto/domain/Money.kt
@@ -1,9 +1,14 @@
 package com.techcourse.openmission.lotto.domain
 
 import com.techcourse.openmission.lotto.domain.constant.Numbers
+import com.techcourse.openmission.lotto.validator.LottoValidator
 
 class Money(
-    val amount:Int
+    val amount: Int
 ) {
+    init {
+        LottoValidator.validateMoney(amount)
+    }
+
     fun lottoPrice(): Int = Numbers.LOTTO_PRICE
 }
