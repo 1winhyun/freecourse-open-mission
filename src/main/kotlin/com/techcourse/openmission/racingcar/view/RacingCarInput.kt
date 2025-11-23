418c5665d021af65858fb10ce1b519ddd590dd42
1winhyun <jimking1@naver.com>
2025-11-23 23:02:02 +0900

    refactor: RacingCar Messages 상수 분리

--- src/main/kotlin/com/techcourse/openmission/racingcar/view/RacingCarInput.kt
+++ src/main/kotlin/com/techcourse/openmission/racingcar/view/RacingCarInput.kt
@@ -1,13 +1,15 @@
 package com.techcourse.openmission.racingcar.view
 
+import com.techcourse.openmission.racingcar.domain.constant.Messages
+
 class RacingCarInput {
     fun inputCarNames(): String {
-        println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)")
+        println(Messages.INPUT_CAR_NAME)
         return readln()
     }
 
     fun inputAttempts(): String {
-        println("시도할 횟수는 몇 회인가요?")
+        println(Messages.INPUT_ATTEMPT)
         return readln()
     }
 }
